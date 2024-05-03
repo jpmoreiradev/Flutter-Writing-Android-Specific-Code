940c9135bc58c785f450297d51dc242d13c7f76c
João Pedro Moreira <jp02120123@gmail.com>
2024-05-03 20:18:14 -0300

    add lib and files

---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final String title;
+
+  MyHomePage({required this.title});
+
+  Future<void> _openBrowser() async {
+    const url = 'https://github.com/jpmoreiradev';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: _openBrowser,
+          child: Text('Open Browser'),
+        ),
+      ),
+    );
+  }
+}
